1be8e4ce665c5c5724bc268b1e3ec6bdd9420f88
4727005 <18295790529@163.com>
2021-04-24 00:11:10 +0800

    dev

--- src/router/index.js
+++ src/router/index.js
@@ -6,67 +6,65 @@ import DefaDultC from '@views/default.vue'
 
 Vue.use(VueRouter)
 
-const routes = [{
-        path: '/',
-        redirect: 'index'
-    }, {
-        path: '/',
-        name: 'Home',
-        components: {
-            nav: TopNav,
-            default: DefaDultC
-        },
+const routes = [
+  {
+    path: '/',
+    redirect: 'index'
+  },
+  {
+    path: '/',
+    name: 'Home',
+    components: {
+      nav: TopNav,
+      default: DefaDultC
+    },
+    meta: {
+      title: '简创'
+    },
+    children: [
+      {
+        path: '/index',
+        name: 'indexP',
+        component: () => import ('../views/index.vue')
+      },
+      {
+        path: '/service',
+        name: 'service',
+        component: () => import ('../views/service.vue'),
         meta: {
-            title: '简创'
-        },
-        children: [{
-                path: '/index',
-                name: 'indexP',
-                component: () =>
-                    import ('../views/index.vue')
-            }, {
-                path: '/service',
-                name: 'service',
-                component: () =>
-                    import ('../views/service.vue'),
-                meta: {
-                    title: '服务'
-                }
-            },
-            {
-                path: '/case',
-                name: 'case',
-                component: () =>
-                    import ('../views/case.vue'),
-                meta: {
-                    title: '案例'
-                }
-            },
-            {
-                path: '/news',
-                name: 'news',
-                component: () =>
-                    import ('../views/news.vue'),
-                meta: {
-                    title: '新闻'
-                }
-            },
-            {
-                path: '/cantact',
-                name: 'cantact',
-                component: () =>
-                    import ('../views/cantact.vue'),
-                meta: {
-                    title: '联系我们'
-                }
-            }
-        ]
-    }
-
+          title: '服务'
+        }
+      },
+      {
+        path: '/case',
+        name: 'case',
+        component: () => import ('../views/case.vue'),
+        meta: {
+          title: '案例'
+        }
+      },
+      {
+        path: '/news',
+        name: 'news',
+        component: () => import ('../views/news.vue'),
+        meta: {
+          title: '新闻'
+        }
+      },
+      {
+        path: '/cantact',
+        name: 'cantact',
+        component: () => import ('../views/cantact.vue'),
+        meta: {
+          title: '联系我们'
+        }
+      }
+    ]
+  }
 ]
 
 const router = new VueRouter({
-    routes
+  routes
 })
 
 export default router
