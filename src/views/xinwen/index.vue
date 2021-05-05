e8ca68d263b5b0343fca4b443c0e63816764ec07
v_changzhifeng <v_changzhifeng@tal.com>
2021-05-05 13:49:28 +0800

    新增新闻页面

--- src/views/xinwen/index.vue
+++ src/views/xinwen/index.vue
@@ -16,7 +16,7 @@
           <span>{{time(item.createTime)[1]}}</span>
           </div>
       </div>
-      <i v-for="item in newsList" :key="item.newsId"></i>
+      <i></i>
     </div>
   </div>
 </template>
@@ -54,61 +54,114 @@ export default {
 </script>
 
 <style lang='less' scoped>
+@media screen and (min-width: 1000px) {
+  .news-list-box {
+    padding: 0 120px;
+    justify-content: space-between;
+    i {
+      width: 387px;
+      margin-right: 20px;
+    }
+    .cur-news-box {
+      width: 387px;
+      margin-right: 20px;
+      .img {
+        width: 387px;
+        height: 220px;
+        margin-bottom: 20px;
+      }
+      .title {
+        font-size: 16px;
+        padding: 0 20px;
+      }
+      .cen-line {
+        width: 24px;
+        height: 4px;
+        margin: 10px 20px 14px;
+      }
+      .subtitle {
+        padding: 0 20px;
+        font-size: 12px;
+      }
+      .createTime {
+        padding: 9px 10px 8px;
+        font-size: 24px;
+        span:last-child {
+          margin-top: 11px;
+        }
+      }
+    }
+  }
+}
+@media screen and (max-width: 1000px) {
+  .news-list-box {
+    padding: 0 15px;
+    justify-content: space-around;
+    i {
+      width: 0;
+    }
+    .cur-news-box {
+      width: 345px;
+      .img {
+        width: 345px;
+        height: 196px;
+        margin-bottom: 10px;
+      }
+      .title {
+        padding: 0 15px;
+        font-size: 14px;
+      }
+      .cen-line {
+        width: 24px;
+        height: 4px;
+        margin: 5px 15px;
+      }
+      .subtitle {
+        padding: 0 15px;
+        font-size: 12px;
+      }
+      .createTime {
+        padding: 10px 9px;
+        font-size: 16px;
+        span:last-child {
+          margin-top: 6px;
+        }
+      }
+    }
+  }
+}
 .news-list-box {
   display: flex;
   flex-wrap: wrap;
-  padding: 0 120px;
-  justify-content: space-between;
   box-sizing: border-box;
-  i {
-    width: 387px;
-    margin-right: 20px;
-  }
   .cur-news-box {
-    width: 387px;
+    margin-top: 20px;
     display: inline-flex;
     flex-direction: column;
     position: relative;
-    margin-right: 20px;
-    flex: 0 1 auto;
-    .img {
-      width: 387px;
-      height: 220px;
-      margin-bottom: 20px;
-    }
     .title {
       display: inline-block;
-      padding: 0 20px;
       box-sizing: border-box;
       color: #333333;
-      font-size: 16px;
     }
     .cen-line {
       box-sizing: border-box;
-      width: 24px;
-      height: 4px;
       background-color: #333333;
-      margin: 10px 20px 14px;
     }
     .subtitle {
       display: inline-block;
-      padding: 0 20px;
       box-sizing: border-box;
       color: #666666;
-      font-size: 12px;
     }
     .createTime {
-      padding: 9px 10px 8px;
       position: absolute;
       top: 0;
       left: 20px;
       background-color: #f1f1f1;
-      font-size: 24px;
       color: #333333;
       text-align: right;
       span:last-child {
         display: inline-block;
-        margin-top: 11px;
       }
     }
   }
